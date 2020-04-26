e47bf7fbc0d59fbac3e929639311854aceb97826
Pa Runzel <parunzel@outlook.de>
2020-04-26 20:30:40 +0200

    Add

---
+++ DiscordBotVampireDiceRoller/Program.cs
@@ -0,0 +1,176 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DiscordBotVampireDiceRoller
+{
+  /// <summary>
+  /// MainClass
+  /// </summary>
+  public class Program
+  {
+    private DiscordSocketClient discordClient;
+
+    private Dictionary<SocketUser, VampireDiceRoll> dicLastRoll4User = new Dictionary<SocketUser, VampireDiceRoll>();
+
+    /// <summary>
+    /// MainMethod
+    /// </summary>
+    /// <param name="args"></param>
+    public static void Main(string[] args)
+    => new Program().MainAsync().GetAwaiter().GetResult();
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public async Task MainAsync()
+    {
+      this.discordClient = new DiscordSocketClient();
+      this.discordClient.Log += Log;
+      this.discordClient.MessageReceived += MessageReceived;
+
+      // Token
+      string strToken = DiscordBotVampireDiceRoller.Properties.Resources.TOKEN;
+
+      await this.discordClient.LoginAsync(TokenType.Bot, strToken);
+      await this.discordClient.StartAsync();
+
+      // Block this task until the program is closed.
+      await Task.Delay(-1);
+    }
+
+    private Task Log(LogMessage msg)
+    {
+      Console.WriteLine(msg.ToString());
+      return Task.CompletedTask;
+    }
+
+    private async Task MessageReceived(SocketMessage message)
+    {
+      if (message.Content.StartsWith(this.discordClient.CurrentUser.Mention))
+      {
+        // The Bot is mentioned in the message - Parse it
+        string strMessage = message.Content.Replace(this.discordClient.CurrentUser.Mention, "").Trim();
+        // The Bot knows roll, reroll and help
+        if (strMessage.StartsWith("roll", StringComparison.OrdinalIgnoreCase))
+        {
+          // Split the string with Blanks
+          string[] strSplit = strMessage.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+          // We're expecting 3 values now
+          if (strSplit.Length == 3)
+          {
+            // We ignore the first one (it should be roll) and cast the rest to integer
+            int intDiceTotal, intDiceRed;
+            if (Int32.TryParse(strSplit[1], out intDiceTotal) == false)
+            {
+              await message.Channel.SendMessageAsync($"{message.Author.Mention} Unable to cast '{strSplit[1]}' to a number");
+            }
+            if (Int32.TryParse(strSplit[2], out intDiceRed) == false)
+            {
+              await message.Channel.SendMessageAsync($"{message.Author.Mention} Unable to cast '{strSplit[2]}' to a number");
+            }
+
+            VampireDiceRoll diceRoll = new VampireDiceRoll(intDiceTotal, intDiceRed);
+            this.dicLastRoll4User[message.Author] = diceRoll;
+
+            await message.Channel.SendMessageAsync($"{message.Author.Mention} {diceRoll.Results}");
+          }
+          else
+          {
+            // Haven't found what we we're looking for
+            await message.Channel.SendMessageAsync($"{message.Author.Mention} Somethings not right with your roll command");
+          }
+        }
+        else if (strMessage.StartsWith("reroll", StringComparison.OrdinalIgnoreCase))
+        {
+          VampireDiceRoll diceRoll;
+          if (this.dicLastRoll4User.TryGetValue(message.Author, out diceRoll))
+          {
+            // Check if the user wants to reroll something specific
+            string[] strSplit = strMessage.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            string strRerollOptions = string.Empty;
+            if (strSplit.Length > 1)
+            {
+              // In the second entry can be specific dice to reroll
+              strRerollOptions = strSplit[1];
+            }
+
+            await message.Channel.SendMessageAsync($"{message.Author.Mention} {diceRoll.Reroll(strRerollOptions)}");
+          }
+          else
+          {
+            // No roll found for the user...
+            await message.Channel.SendMessageAsync($"{message.Author.Mention} I found no roll for you to reroll");
+          }
+        }
+        else if (strMessage.StartsWith("help", StringComparison.OrdinalIgnoreCase))
+        {
+          await message.Channel.SendMessageAsync($"{message.Author.Mention} You can tell me to roll the dice by telling me \"roll {{number of total dice}} {{number of red dice}}\" (\"roll 7 2\")" +
+            $"{Environment.NewLine}or \"reroll\" to reroll up to three normal dice from your last roll. Without specification a reroll will reroll failures. " +
+            $"You can specify what you want to reroll by adding 'c' for a critical, 's' for an success or 'f' for a failure (reroll cff).");
+        }
+        else
+        {
+          await message.Channel.SendMessageAsync($"{message.Author.Mention} I don't understand. Type \"help\" when you need some help from me");
+        }
+      }
+
+      if (message.Author.IsBot == false)
+      {
+        // Response to see, that the Bot is still responding
+        if (message.Content.Contains("test", StringComparison.OrdinalIgnoreCase))
+        {
+          await message.Channel.SendMessageAsync("Nein!");
+        }
+        else if (message.Content.Contains("doch", StringComparison.OrdinalIgnoreCase))
+        {
+          await message.Channel.SendMessageAsync("Oh!");
+        }
+        else if (message.Content.Contains("frank", StringComparison.OrdinalIgnoreCase))
+        {
+          await message.Channel.SendMessageAsync("https://tenor.com/view/drac-dracula-vampire-horror-shadow-gif-4888147");
+        }
+        else if (message.Content.Contains("federhauser", StringComparison.OrdinalIgnoreCase))
+        {
+          await message.Channel.SendMessageAsync("https://tenor.com/view/vampire-dracula-mr-burns-fangs-gif-5730463");
+        }
+        else if (message.Content.Contains("nacht", StringComparison.OrdinalIgnoreCase))
+        {
+          await message.Channel.SendMessageAsync("https://tenor.com/view/nesforatu-dracula-goodnight-gif-10044942");
+        }
+        else if (message.Content.Contains("morgen", StringComparison.OrdinalIgnoreCase))
+        {
+          await message.Channel.SendMessageAsync("https://tenor.com/view/wake-up-dracula-hangover-leslie-nielsen-gif-14211396");
+        }
+        else if (message.Content.Contains("cool", StringComparison.OrdinalIgnoreCase))
+        {
+          await message.Channel.SendMessageAsync("https://tenor.com/view/bela-lugosi-dracula-vampire-halloween-horror-gif-15453044");
+        }
+        else if (message.Content.Contains("zahl", StringComparison.OrdinalIgnoreCase) || message.Content.Contains("zählen", StringComparison.OrdinalIgnoreCase))
+        {
+          await message.Channel.SendMessageAsync("https://tenor.com/view/number-of-the-day-sesame-street-gif-14837388");
+        }
+        else if (message.Content.Contains("ronja", StringComparison.OrdinalIgnoreCase))
+        {
+          await message.Channel.SendMessageAsync("https://tenor.com/view/cat-bite-gif-7748718");
+        }
+        else if (message.Content.Contains("biss", StringComparison.OrdinalIgnoreCase) || message.Content.Contains("beißen", StringComparison.OrdinalIgnoreCase) || message.Content.Contains("beissen", StringComparison.OrdinalIgnoreCase))
+        {
+          await message.Channel.SendMessageAsync("https://tenor.com/view/kermit-vampire-gif-3563348");
+        }
+        else if (message.Content.Contains("nobody", StringComparison.OrdinalIgnoreCase))
+        {
+          await message.Channel.SendMessageAsync("https://tenor.com/view/alan-walker-hacker-gif-7953330");
+        }
+        else if (message.Content.Contains("dracula", StringComparison.OrdinalIgnoreCase))
+        {
+          await message.Channel.SendMessageAsync("https://tenor.com/view/hellsing-vampire-anime-gif-9676269");
+        }
+      }
+    }
+  }
+}
---
+++ DiscordBotVampireDiceRoller/VampireDiceRoll.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiscordBotVampireDiceRoller
+{
+  /// <summary>
+  /// Class for a VampireDiceRoll
+  /// </summary>
+  public class VampireDiceRoll
+  {
+    private List<int> lstResultsNormal, lstResultsRed;
+    private Random randomizer;
+    private bool bolRerolled = false;
+
+    /// <summary>
+    /// Konstruktor - Takes the number of total dice and red dice
+    /// </summary>
+    /// <param name="_diceTotal"></param>
+    /// <param name="_diceRed"></param>
+    public VampireDiceRoll(int _diceTotal, int _diceRed)
+    {
+      // Initialize the Randomizer
+      this.randomizer = new Random((int)DateTime.Now.Ticks);
+
+      // Initialize the resultslists
+      this.lstResultsNormal = new List<int>(_diceTotal - _diceRed);
+      this.lstResultsRed = new List<int>(_diceRed);
+
+      // Do the rolling
+      for (int intCount = 0; intCount < _diceTotal - _diceRed; intCount++)
+      {
+        this.lstResultsNormal.Add(this.randomizer.Next(1, 11));
+      }
+
+      for (int intCount = 0; intCount < _diceRed; intCount++)
+      {
+        this.lstResultsRed.Add(this.randomizer.Next(1, 11));
+      }
+    }
+
+    /// <summary>
+    /// Rerolling
+    /// </summary>
+    /// <param name="_rerollOptions"></param>
+    /// <returns></returns>
+    public string Reroll(string _rerollOptions)
+    {
+      if (this.bolRerolled == false)
+      {
+        // We can only reroll up to 3 dices
+        int intRerolls = 0;
+
+        if (string.IsNullOrEmpty(_rerollOptions))
+        {
+          // Iterate over the normal dices
+          for (int intIndex = 0; intIndex < this.lstResultsNormal.Count; intIndex++)
+          {
+            // Check if not a success
+            if (intRerolls < 3 && this.lstResultsNormal[intIndex] < 6)
+            {
+              this.lstResultsNormal[intIndex] = this.randomizer.Next(1, 11);
+              intRerolls++;
+            }
+          }
+        }
+        else
+        {
+          
+          List<int> lstIndexRerolledDice = new List<int>();
+
+          // First Check if there are reroll options
+          foreach (char optionAktuell in _rerollOptions)
+          {
+            // Iterate over the normal dices
+            for (int intIndex = 0; intIndex < this.lstResultsNormal.Count; intIndex++)
+            {
+              Func<int, bool> funcCompare;
+              if (char.ToUpperInvariant(optionAktuell) == char.ToUpperInvariant('c'))
+              {
+                funcCompare = (intValue => intValue == 10);
+              }
+              else if (char.ToUpperInvariant(optionAktuell) == char.ToUpperInvariant('s'))
+              {
+                funcCompare = (intValue => intValue > 5);
+              }
+              else if (char.ToUpperInvariant(optionAktuell) == char.ToUpperInvariant('f'))
+              {
+                funcCompare = (intValue => intValue < 6);
+              }
+              else
+              {
+                continue;
+              }
+              // Check if the option is met
+              if (intRerolls < 3 && lstIndexRerolledDice.Contains(intIndex) == false && funcCompare(this.lstResultsNormal[intIndex]))
+              {
+                this.lstResultsNormal[intIndex] = this.randomizer.Next(1, 11);
+                lstIndexRerolledDice.Add(intIndex);
+                intRerolls++;
+                break;
+              }
+            }
+          }
+        }
+        
+
+        this.bolRerolled = true;
+
+        return Results;
+      }
+      else
+      {
+        // Already rerolled
+        return "Already rerolled";
+      }
+    }
+
+    /// <summary>
+    /// Gets a formated string with the resulst from the roll
+    /// </summary>
+    public string Results
+    {
+      get
+      {
+        // Run over the results and count everything
+        int intNormalFailures = 0;
+        int intSuccesses = 0;
+        int intPossibleCrits = 0;
+        int intRedCrits = 0;
+        int intRedZeros = 0;
+
+        foreach (int result in this.lstResultsNormal)
+        {
+          if (result > 5)
+          {
+            intSuccesses++;
+          }
+          else
+          {
+            intNormalFailures++;
+          }
+          if (result == 10)
+          {
+            intPossibleCrits++;
+          }
+        }
+        foreach (int result in this.lstResultsRed)
+        {
+          if (result == 1)
+          {
+            intRedZeros++;
+          }
+          if (result > 5)
+          {
+            intSuccesses++;
+          }
+          if (result == 10)
+          {
+            intPossibleCrits++;
+            intRedCrits++;
+          }
+        }
+
+        StringBuilder strBuilderReturn = new StringBuilder();
+        strBuilderReturn.Append("Successes: ");
+        strBuilderReturn.Append(intSuccesses + (intPossibleCrits / 2 * 2));
+        if (intPossibleCrits > 1)
+        {
+          if (intRedCrits > 0)
+          {
+            strBuilderReturn.Append(" | **Messy critical**");
+          }
+          else
+          {
+            strBuilderReturn.Append(" | **Critical**");
+          }
+        }
+
+        if (intRedZeros > 0)
+        {
+          strBuilderReturn.Append(" | **Possible beastial failure**");
+        }
+
+        strBuilderReturn.Append(" | Failures to reroll: ");
+        strBuilderReturn.Append(intNormalFailures);
+
+        strBuilderReturn.Append(" | Detailed results: ");
+        foreach (int result in this.lstResultsNormal)
+        {
+          if (result < 6)
+          {
+            strBuilderReturn.Append("F");
+          }
+          else if (result < 10)
+          {
+            strBuilderReturn.Append("S");
+          }
+          else
+          {
+            strBuilderReturn.Append("C");
+          }
+        }
+        strBuilderReturn.Append(" ");
+        foreach (int result in this.lstResultsRed)
+        {
+          if (result < 2)
+          {
+            strBuilderReturn.Append("B");
+          }
+          else if (result < 6)
+          {
+            strBuilderReturn.Append("F");
+          }
+          else if (result < 10)
+          {
+            strBuilderReturn.Append("S");
+          }
+          else
+          {
+            strBuilderReturn.Append("C");
+          }
+        }
+
+        return strBuilderReturn.ToString();
+      }
+    }
+  }
+}
